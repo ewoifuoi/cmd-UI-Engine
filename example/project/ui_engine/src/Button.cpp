8ba43625529ac426f9b1f651ee4c1d7400959058
ewoifuoi <hamimelons@126.com>
2022-04-08 14:18:38 +0800

    使用 function<void ()> func

--- example/project/ui_engine/src/Button.cpp
+++ example/project/ui_engine/src/Button.cpp
@@ -26,7 +26,7 @@ Button::Button(string Tex, int x, int y, int c, int oc) {
     this->show();
 }
 
-Button::Button(string Tex, int x, int y, int c, int oc, void(*func)()) {
+Button::Button(string Tex, int x, int y, int c, int oc, function<void()> func) {
     text = Tex;
     loc.first = x; loc.second = y;
     color = c;
